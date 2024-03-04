665ff052fd9f8258f2203ad3529c0b32a0bf4bbe
diam0ndkiller <118304462+diam0ndkiller@users.noreply.github.com>
2024-03-04 23:30:36 +0100

    Update show-content-overview.php

--- scripts/show-content-overview.php
+++ scripts/show-content-overview.php
@@ -2,18 +2,8 @@
 						<td style="vertical-align: top;">
 							<h2 style="text-align: center">Content</h2>
 							<div class="b">
-								<h3>Naturemag</h3>
-								<p class="ntmg"><a href="naturemag/en/home">Naturemag Home</a></p>
-								<p class="ntmg"><a href="naturemag/en/downloads">Naturemag Files</a></p>
-								<p class="ntmg"><a href="naturemag/en/wiki">Naturemag Wiki</a></p>
-							</div>
-							<div class="b">
 								<h3>NASPI</h3>
 								<p class="naspi"><a href="naspi/?theme=<?php echo $theme;?>">Browse Network Attached Storage</a></p>
 								<p class="naspi"><a href="naspi/player.php?theme=<?php echo $theme;?>">Play Media on the NAS</a></p>
-							</div>
-							<div class="b">
-								<h3>moviedb</h3>
-								<p class="moviedb"><a href="moviedb/?theme=<?php echo $theme;?>">Database of DVD movies</a></p>
 							</div>
 						</td>
