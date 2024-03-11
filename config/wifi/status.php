4c49318f280c554af9734dbdbcb7210931aa6aad
diam0ndkiller <118304462+diam0ndkiller@users.noreply.github.com>
2024-03-11 08:14:55 +0100

    Update status.php

--- config/wifi/status.php
+++ config/wifi/status.php
@@ -56,7 +56,7 @@
 							<?php
 								if(isset($_POST['restart'])) {
 									shell_exec("sudo -S ../sudoscript.py restart-hostapd 2>&1");
-									echo "<p class='content' style='color: #00aa00; margin: 0px;'> Successfully restarted <tt>hostapd</tt></p>";
+									echo "<p class='content' style='color: #00aa00; margin: 0px;'> Successfully restarted the <tt>iftop</tt> service</p>";
 								}
 							?>
 						</td>
